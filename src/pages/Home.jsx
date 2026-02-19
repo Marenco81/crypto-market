d2c713392c317e8e7308bd95749dcf8c8bfae4e7
Alvaro Marenco <alvaromarencom@gmail.com>
2026-02-19 13:12:45 -0600

    Crypto Card UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
+import { fetchCryptos } from "../api/coinGecko"
+import { CryptoCard } from "../components/CryptoCard";
 
 
 export const Home = () => {
+
+    const [cryptoList, setCryptoList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetchCryptoData();
+    }, []);
+
+    const fetchCryptoData = async () => {
+        
+        try {
+            const data = await fetchCryptos();
+            setCryptoList(data);
+            
+        } catch (error) {
+            console.error("Error fetching crypto info:", error)
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
   return (
-    <div>
-      This is the Home Page
+    <div className="app">
+      {isLoading ? (
+        <div className="loading">
+            <div className="spinner"></div>
+            <p>Loading crypto data...</p>
+        </div> 
+        ) : (
+        <div className="crypto-container">
+            {cryptoList.map((crypto, key) => (
+                <CryptoCard crypto={crypto} key={key}></CryptoCard>
+            ) )}
+        </div>
+        )}
     </div>
   )
 }
